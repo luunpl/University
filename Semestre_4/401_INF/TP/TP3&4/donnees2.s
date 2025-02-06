59f12921c566033e6e7bfdc6edfe7d2c8501b1d4
luunpl <luunguyenphuocloc@gmail.com>
2025-02-06 18:03:32 +0100

    Daily Commit

---
+++ Semestre_4/401_INF/TP/TP3&4/donnees2.s
@@ -0,0 +1,9 @@
+.data
+aa: .word 0x00000041    @ 65 en 32 bits
+oo: .word 0x0000000f    @ 15 en 32 bits
+cc: .asciz "bonjour"
+rr: .word 0x00000042    @ 66 en 32 bits
+    .word 0x00000003    @ 3 en 32 bits
+T: .word 0x11223456     @ 0x1122 et 0x3456 combinés en 32 bits
+   .word 0xfafd0000     @ 0xfafd en 32 bits
+xx: .word 0x00000041    @ 65 en 32 bits
