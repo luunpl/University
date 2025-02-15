aa7ad797c97d75f74b9a41b134f03d8ca5c4fd2f
luunpl <luunguyenphuocloc@gmail.com>
2025-02-15 13:23:13 +0100

    Remote Commit

--- Semestre_4/401_INF/TP/TP3&4/accesmem2.s
+++ Semestre_4/401_INF/TP/TP3&4/accesmem2.s
@@ -1,4 +1,4 @@
-.data
+    .data
 D1:    .word  266
 D2:    .hword 42
 D3:    .byte 12
@@ -8,21 +8,12 @@ D3:    .byte 12
 main:
       LDR r3, LD_D1
       LDR r4, [r3]
-      BL EcrNdecimal32  @ Affiche la valeur de r4 (266)
-      MOV r1, r3
-      BL EcrNdecimal32  @ Affiche l'adresse de D1
 
       LDR r5, LD_D2
       LDRH r6, [r5]
-      BL EcrNdecimal16  @ Affiche la valeur de r6 (42)
-      MOV r1, r5
-      BL EcrNdecimal32  @ Affiche l'adresse de D2
 
       LDR r7, LD_D3
       LDRB r8, [r7]
-      BL EcrNdecimal8   @ Affiche la valeur de r8 (12)
-      MOV r1, r7
-      BL EcrNdecimal32  @ Affiche l'adresse de D3
 
 fin:  B exit  @ terminaison immédiate du processus (plus tard on saura faire mieux)
 
--- Semestre_4/401_INF/TP/TP3&4/ecrmem.s
+++ Semestre_4/401_INF/TP/TP3&4/ecrmem.s
@@ -1,4 +1,4 @@
-.data
+    .data
 DW:    .word  0
 DH:    .hword 0
 DB:    .byte 0
@@ -6,56 +6,19 @@ DB:    .byte 0
     .text
     .global main
 main:
-      @ Affichage de la valeur initiale à l'adresse DW
       LDR r0, LD_DW
       LDR r1, [r0]
       BL EcrHexa32
       BL EcrZdecimal32
 
-      @ Modification de la valeur à l'adresse DW
       MOV r4, #-10
       LDR r5, LD_DW
       STR r4, [r5]
 
-      @ Affichage de la nouvelle valeur à l'adresse DW
       LDR r0, LD_DW
       LDR r1, [r0]
       BL EcrHexa32
       BL EcrZdecimal32
-
-      @ Affichage de la valeur initiale à l'adresse DH
-      LDR r0, LD_DH
-      LDRH r1, [r0]
-      BL EcrHexa32
-      BL EcrZdecimal16
-
-      @ Modification de la valeur à l'adresse DH
-      MOV r4, #42
-      LDR r5, LD_DH
-      STRH r4, [r5]
-
-      @ Affichage de la nouvelle valeur à l'adresse DH
-      LDR r0, LD_DH
-      LDRH r1, [r0]
-      BL EcrHexa32
-      BL EcrZdecimal16
-
-      @ Affichage de la valeur initiale à l'adresse DB
-      LDR r0, LD_DB
-      LDRB r1, [r0]
-      BL EcrHexa32
-      BL EcrZdecimal8
-
-      @ Modification de la valeur à l'adresse DB
-      MOV r4, #12
-      LDR r5, LD_DB
-      STRB r4, [r5]
-
-      @ Affichage de la nouvelle valeur à l'adresse DB
-      LDR r0, LD_DB
-      LDRB r1, [r0]
-      BL EcrHexa32
-      BL EcrZdecimal8
 
 fin:  B exit  @ terminaison immédiate du processus (plus tard on saura faire mieux)
 
