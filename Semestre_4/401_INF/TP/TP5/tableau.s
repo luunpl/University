bb984b9668c17105029d8b82bf2bed04fd45516a
luunpl <luunguyenphuocloc@gmail.com>
2025-02-20 13:19:45 +0100

    Remote Commit

--- Semestre_4/401_INF/TP/TP5/tableau.s
+++ Semestre_4/401_INF/TP/TP5/tableau.s
@@ -12,11 +12,20 @@ main:
    mov r1, #22
    add r0, r0, #4
    str r1, [r0]
-   
 
-   @ a completer
+   @ Partie a Completer
+   mov r1, #33
+   add r0, r0, #4
+   str r1, [r0]
+
+   mov r1, #44
+   add r0, r0, #4
+   str r1, [r0]
+
+   mov r1, #55
+   add r0, r0, #4
+   str r1, [r0]
 
 fin:  BX LR
 
 ptr_debutTAB : .word debutTAB
-
