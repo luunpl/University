0161addf5c8c84b7a9a7ffa10b9a2e928d4e54ae
luunpl <luunguyenphuocloc@gmail.com>
2025-04-08 08:01:24 +0200

    Projet INF404

--- Semestre_4/404_INF/Interpreter/analyse_syntaxique.c
+++ Semestre_4/404_INF/Interpreter/analyse_syntaxique.c
@@ -10,14 +10,13 @@
 
 Couple TS[NBMAXSYMB];
 int NbSymb;
-int c = 1; //condition pour pas executer inst avant appel a interpreter_si_alors_sinon()
-
+int c = 1;
 
 void Rec_pgm(Ast *resultat) {
     Rec_seq_inst(resultat);
 }
 
-void type_cond(TypeCond *cond){
+void type_cond(TypeCond *cond) {
     char *lexeme = lexeme_courant().chaine; 
 
     if (strcmp(lexeme, "<=") == 0) {
@@ -39,22 +38,21 @@ void type_cond(TypeCond *cond){
 }
 
 void Rec_condition(Ast *Acond) {
-    Ast Ag,Ad;
+    Ast Ag, Ad;
     TypeCond cond;
     Rec_eag(&Ag);
     if (lexeme_courant().nature == OPCOMP) {
-        //printf("condition = %s\n",lexeme_courant().chaine);
         type_cond(&cond);
-    //printf("Condition = %d\n",cond);
         avancer();
         Rec_eag(&Ad);
-        *Acond = creer_cond(cond,Ag,Ad);
+        *Acond = creer_cond(cond, Ag, Ad);
     } else {
         printf("Erreur: Opérateur de comparaison manquant\n");
         exit(1);
     }
 }
-void Rec_seq_inst (Ast *resultat) {
+
+void Rec_seq_inst(Ast *resultat) {
     Ast A1;
     Rec_inst(&A1);
     Rec_suite_seq_inst(A1, resultat);
@@ -75,9 +73,7 @@ void Rec_suite_seq_inst(Ast donnee, Ast *resultat) {
 }
 
 void Rec_inst(Ast *resultat) {
-    Ast Ag ,Ad, Acond, Athen, Aelse,Abody,Acond2;
-    //printf("lexeme courant nature: %d\n", lexeme_courant().nature);   // pour débugger, à décommenter
-    //afficheTS(TS, NbSymb);          // pour débugger
+    Ast Ag, Ad, Acond, Athen, Aelse, Abody, Acond2;
     switch (lexeme_courant().nature) {
         case IDF:
             Ag = creer_idf(lexeme_courant().chaine);
@@ -86,14 +82,15 @@ void Rec_inst(Ast *resultat) {
                 avancer();
                 Rec_eag(&Ad);
                 *resultat = creer_aff(Ag, Ad);
-                if(c==1){
-                interpreter_aff(*resultat);
+                if (c == 1) {
+                    interpreter_aff(*resultat);
+                } else {
+                    if (estPresentTS2(Ag->ident, TS, NbSymb) == 0) {          
+                        insererTS(Ag->ident, 0, TS, &NbSymb);
+                    }
                 }
-                else{
-                    if(estPresentTS2(Ag->ident,TS,NbSymb)==0){          
-                    insererTS(Ag->ident,0,TS,&NbSymb);}}
             } else {
-                printf("Erreur: Instruction invalide (pas de AFF suivant IDF) ou mot-clé invalide\n");
+                printf("Erreur: Instruction invalide ou mot-clé invalide\n");
                 exit(1);
             }
             break;
@@ -105,21 +102,20 @@ void Rec_inst(Ast *resultat) {
                     Ag = creer_idf(lexeme_courant().chaine);
                     avancer();
                     *resultat = creer_lire(Ag);
-
-                if (lexeme_courant().nature == PARF) {
-                    avancer();
-                    if(lexeme_courant().nature == SEPAFF){
-                        if(c==1){
-                            interpreter_lire(*resultat);
+                    if (lexeme_courant().nature == PARF) {
+                        avancer();
+                        if (lexeme_courant().nature == SEPAFF) {
+                            if (c == 1) {
+                                interpreter_lire(*resultat);
+                            }
+                        } else {
+                            printf("Erreur : ';' manquant\n");
+                            exit(1);
                         }
-                    } else{
-                        printf("Erreur : ';' manquant\n");
+                    } else {
+                        printf("Erreur: Parenthèse fermante manquante\n");
                         exit(1);
                     }
-                } else {
-                    printf("Erreur: Parenthèse fermante manquante\n");
-                    exit(1);
-                }
                 } else {
                     printf("Erreur: Identificateur manquant\n");
                     exit(1);
@@ -135,19 +131,16 @@ void Rec_inst(Ast *resultat) {
                 avancer();
                 Rec_eag(&Ag);
                 *resultat = creer_ecrire(Ag);
-
                 if (lexeme_courant().nature == PARF) {
                     avancer();
-                if(lexeme_courant().nature == SEPAFF){
-                if(c==1){
-                    interpreter_ecrire(*resultat);
-                }
-                    //avancer();
-                }
-                else{
-                    printf("Erreur : ';' manquant\n");
-                    exit(1);
-                }
+                    if (lexeme_courant().nature == SEPAFF) {
+                        if (c == 1) {
+                            interpreter_ecrire(*resultat);
+                        }
+                    } else {
+                        printf("Erreur : ';' manquant\n");
+                        exit(1);
+                    }
                 } else {
                     printf("Erreur: Parenthèse fermante manquante\n");
                     exit(1);
@@ -160,28 +153,21 @@ void Rec_inst(Ast *resultat) {
         case SI:
             avancer();
             Rec_condition(&Acond);
-
-            // printf("c = %d\n",c);
-            if (lexeme_courant().nature==ALORS) {
+            if (lexeme_courant().nature == ALORS) {
                 avancer();
                 c--;
                 Rec_seq_inst(&Athen);
-                if (lexeme_courant().nature==SINON) {
+                if (lexeme_courant().nature == SINON) {
                     avancer();
                     Rec_seq_inst(&Aelse);
-                    if (lexeme_courant().nature==FSI) {
-
-
+                    if (lexeme_courant().nature == FSI) {
                         *resultat = creer_if(Acond, Athen, Aelse);
                         avancer();
-                        if(lexeme_courant().nature != SEPAFF){
+                        if (lexeme_courant().nature != SEPAFF) {
                             interpreter_si_alors_sinon(*resultat);
-                            c=1;
+                            c = 1;
                         }
-
-                        }
-                    else
-                    {
+                    } else {
                         printf("Erreur: FSI manquant\n");
                         exit(1);
                     }
@@ -197,54 +183,42 @@ void Rec_inst(Ast *resultat) {
         case TANQUE:
             avancer();
             Rec_condition(&Acond2);
-            //c = valeur_booleenne(Acond2);
-            if(lexeme_courant().nature == FAIRE){
+            if (lexeme_courant().nature == FAIRE) {
                 avancer();
-                c=0;
+                c = 0;
                 Rec_seq_inst(&Abody);
-                if(lexeme_courant().nature == FAIT){
+                if (lexeme_courant().nature == FAIT) {
                     avancer();
-                    //printf("avant\n");
-                    //afficheTS(TS,NbSymb);
-
-                    if(lexeme_courant().nature== SEPAFF ){
-                        *resultat = creer_while(Acond2,Abody);
+                    if (lexeme_courant().nature == SEPAFF) {
+                        *resultat = creer_while(Acond2, Abody);
                         interpreter_while(*resultat);
-                        c=1;
+                        c = 1;
                         avancer();
-                    }
-                    else{
+                    } else {
                         printf("Erreur : ';' manquant\n");
                         exit(1);
                     }
-                }
-                else{
+                } else {
                     printf("Erreur : FAIT manquant\n");
                     exit(1);
                 }
-            }
-            else{
+            } else {
                 printf("Erreur : FAIRE manquant\n");
                 exit(1);
             }
-            
+            break;
         default:
-            //printf("Erreur: Instruction invalide (rec_inst)\n");
-            //exit(1);
             break;
     }
 }
+
 void Rec_facteur(Ast *resultat) {
     int v;
-    int trouve;              // pour savoir si l'IDF est présent dans la TS
+    int trouve;
     switch (lexeme_courant().nature) {
     case ENTIER:
-        *resultat = creer_valeur(lexeme_courant().valeur,lexeme_courant().chaine);
+        *resultat = creer_valeur(lexeme_courant().valeur, lexeme_courant().chaine);
         avancer();
-        /*if(lexeme_courant().nature == ENTIER){
-            printf("erreur syntaxique\n");
-            exit(1);
-        } */
         break;
     case PARO:
         avancer();
@@ -260,19 +234,16 @@ void Rec_facteur(Ast *resultat) {
         avancer();
         Ast A1;
         Rec_facteur(&A1);
-        *resultat = creer_op_unaire(U_MOINS,A1);
+        *resultat = creer_op_unaire(U_MOINS, A1);
         break;
     case IDF:
-        //printf("lexeme courant: %s\n", lexeme_courant().chaine);
-        //printf("lexeme courant nature: %d\n", lexeme_courant().nature);
-        //afficheTS(TS, NbSymb);
         trouve = estPresentTS(lexeme_courant().chaine, &v, TS, NbSymb);
-        if (trouve==1) {
-            *resultat = creer_valeur(v,lexeme_courant().chaine);
+        if (trouve == 1) {
+            *resultat = creer_valeur(v, lexeme_courant().chaine);
         } else {
-            printf("erreur variable n'est pas définie\n");
-            exit(1 );
-        }                                       // à décommenter
+            printf("Erreur: Variable non définie\n");
+            exit(1);
+        }
         avancer();
         break;
     default:
@@ -282,8 +253,7 @@ void Rec_facteur(Ast *resultat) {
     }
 }
 
-void Rec_terme(Ast *resultat)
-{
+void Rec_terme(Ast *resultat) {
     Rec_seq_facteur(resultat);
 }
 
@@ -304,17 +274,15 @@ void Rec_suite_seq_terme(Ast donnee, Ast *resultat) {
     }
 }
 
-void Rec_seq_terme(Ast* resultat) {
+void Rec_seq_terme(Ast *resultat) {
     Ast A1;
     Rec_terme(&A1);
     Rec_suite_seq_terme(A1, resultat);
 }
 
-void Rec_eag(Ast *resultat)
-{
+void Rec_eag(Ast *resultat) {
     Rec_seq_terme(resultat);
 }
-
 
 void Rec_suite_seq_facteur(Ast donnee, Ast *resultat) {
     TypeOperateur Op;
@@ -333,37 +301,31 @@ void Rec_suite_seq_facteur(Ast donnee, Ast *resultat) {
     }
 }
 
-
 void Rec_seq_facteur(Ast *resultat) {
     Ast A1;
     Rec_facteur(&A1);
     Rec_suite_seq_facteur(A1, resultat);
 }
 
-
-void Rec_op1(TypeOperateur *Op)
-{
-    switch (lexeme_courant().nature)
-    {
+void Rec_op1(TypeOperateur *Op) {
+    switch (lexeme_courant().nature) {
     case PLUS:
         *Op = N_PLUS;
         avancer();
         break;
     case MOINS:
-       * Op = N_MOINS;
+        *Op = N_MOINS;
         avancer();
         break;
     default:
-        printf("erreur\n");
+        printf("Erreur\n");
         exit(0);
         break;
     }
 }
 
-void Rec_op2(TypeOperateur *Op)
-{
-    switch (lexeme_courant().nature)
-    {
+void Rec_op2(TypeOperateur *Op) {
+    switch (lexeme_courant().nature) {
     case MUL:
         *Op = N_MUL;
         avancer();
@@ -373,61 +335,51 @@ void Rec_op2(TypeOperateur *Op)
         avancer();
         break;
     default:
-        printf("erreur\n");
+        printf("Erreur\n");
         exit(0);
         break;
     }
 }
 
-void Rec_aff(Ast *resultat) 
-{
+void Rec_aff(Ast *resultat) {
     Ast A; 
-   // int v;              // valeur de IDF
-    char idf[LIDF];     //nom de IDF
+    char idf[LIDF];
 
-    if (lexeme_courant().nature==IDF) {
+    if (lexeme_courant().nature == IDF) {
         strcpy(idf, lexeme_courant().chaine);
         avancer();
-        if (lexeme_courant().nature==AFF) {
+        if (lexeme_courant().nature == AFF) {
             avancer();
             Rec_eag(&A);
-            //afficher_ast(A);
             interpreter_aff(A);
-            if (lexeme_courant().nature==SEPAFF) {
+            if (lexeme_courant().nature == SEPAFF) {
                 avancer();
             } else {
-                printf("erreur rec aff\n");
+                printf("Erreur Rec_aff\n");
                 exit(0);
             }
         } else {
-            printf("erreur rec aff\n");
+            printf("Erreur Rec_aff\n");
             exit(0);
         }
     } else {
-        printf("erreur rec aff\n");
+        printf("Erreur Rec_aff\n");
         exit(0);
     }
 }
 
-void Rec_seq_aff(Ast *resultat)
-{
+void Rec_seq_aff(Ast *resultat) {
     if (lexeme_courant().nature == AFF) {
         NbSymb = initTS();
         Rec_aff(resultat);
         Rec_seq_aff(resultat);
-    } else {
-        // cas de epsilon
     }
 }
 
-void analyser(char *fichier, Ast *arbre)
-{
-    demarrer(fichier);   // demarre analyse lexical
-    while (! fin_de_sequence())
-    {
+void analyser(char *fichier, Ast *arbre) {
+    demarrer(fichier);
+    while (!fin_de_sequence()) {
         Rec_pgm(arbre);
     }  
-   //afficheTS(TS, NbSymb);            // pour vérifier tous les affectations dans le tableau, à commenter
     arreter();
-
 }
